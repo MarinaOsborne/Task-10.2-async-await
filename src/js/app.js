e3a21a258c3b9c28aecb29316bc3314b687d980f
Marina Osborne <marina08088@mail.ru>
2021-01-30 21:58:15 +0300

    done

--- src/js/app.js
+++ src/js/app.js
@@ -3,14 +3,12 @@ import json from './parser';
 import read from './reader';
 
 export default class GameSavingLoader {
-  static load() {
-    const data = read();
-    return data
-      .then((response) => {
-        const saving = json(response);
-        return saving;
-      }).catch((error) => {
-        console.log(error);
-      });
+  static async load() {
+    try {
+      const response = await read();
+      return json(response);
+    } catch (error) {
+      return error;
+    }
   }
 }

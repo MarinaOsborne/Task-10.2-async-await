41a2f85c9c71b9f035de6bef71e1fdf5a740b4e2
Marina Osborne <marina08088@mail.ru>
2021-01-30 20:45:59 +0300

    done

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,45 +1,6 @@
-import MathChar, { Magician, Daemon } from '../app';
-import Character from '../character';
+import GameSavingLoader from '../app';
 
-test('проверка attack', () => {
-  const received = new Magician('Иван', 'Magician', true, 8);
-  expect(received.attack).toEqual(55);
-});
-
-test('проверка distance', () => {
-  const received = new Magician('Иван', 'Magician', true, 18);
-  expect(received.attack).toEqual(0);
-});
-
-test('проверка stoned false', () => {
-  const received = new Magician('Иван', 'Magician', false, 3);
-  expect(received.attack).toEqual(20);
-});
-
-test('проверка stoned', () => {
-  const received = new Magician('Иван', 'Magician', false, 3);
-  expect(received.stoned).toEqual(false);
-});
-
-test('проверка attack', () => {
-  const received = new Daemon('Иван', 'Daemon', true, 8);
-  expect(received.attack).toEqual(55);
-});
-
-test('проверка длины имени <3', () => {
-  expect(() => {
-    new Character('хз', 'Swordsman', 10, 40);
-  }).toThrow();
-});
-
-test('проверка длины имени >10', () => {
-  expect(() => {
-    new Character('хзuhhgfhfhfjjjsd', 'Swordsman', 10, 40);
-  }).toThrow();
-});
-
-test('проверка string', () => {
-  expect(() => {
-    new Character(7, 'Swordsman', 10, 40);
-  }).toThrow();
+test('Check GameSavingLoader', async () => {
+  const data = await GameSavingLoader.load();
+  expect(data).toEqual('{"id":9,"created":1546300800,"userInfo":{"id":1,name":"Hitman","level":10,"points":2000}}');
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,45 +1,16 @@
-// // console.log('worked');
-import Character from './character';
+//  console.log('worked');
+import json from './parser';
+import read from './reader';
 
-export default class MathChar extends Character {
-  constructor(name, type, stoned, distance) {
-    super(name, type);
-    this._stoned = stoned;
-    this.distance = distance;
-    this._attack = 100;
-    this.defence = 40;
-  }
-
-  get stoned() {
-    return this._stoned;
-  }
-
-  set stoned(value) {
-    this._stoned = value;
-  }
-
-  get attack() {
-    const attack = this._attack - (this._attack * (1 - ((this.distance - 1) / 10)));
-    if (this.distance > 10) return 0;
-    if (!this._stoned) {
-      return attack;
-    }
-    return attack < 0 ? 0 : Math.round(attack - Math.log2(this.distance) * 5);
-  }
-
-  set attack(attack) {
-    this._attack = attack;
-  }
-}
-
-export class Magician extends MathChar {
-  constructor(...args) {
-    super(...args, 'Magician');
-  }
-}
-
-export class Daemon extends MathChar {
-  constructor(...args) {
-    super(...args, 'Daemon');
+export default class GameSavingLoader {
+  static load() {
+    const data = read();
+    return data
+      .then((response) => {
+        const saving = json(response);
+        return saving;
+      }).catch((error) => {
+        console.log(error);
+      });
   }
 }
